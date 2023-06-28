881239b6a2e18ff8cca4e56d45514616f118064e
mamemamema <st.msnr.wk@gmail.com>
2023-06-28 14:02:51 +0900

    test.sh mock enable/disable param add

--- myappback/test.sh
+++ myappback/test.sh
@@ -9,11 +9,22 @@ export ARCHIVES_COUNTER_NAME=archives
 export TODOS_COUNTER_NAME=todos
 export TASKS_COUNTER_NAME=tasks
 
-./run_dynamodb.sh &
+while getopts 'm:' flag; do
+    case "${flag}" in
+        m) WITH_MOCK=${OPTARG} ;;
+    esac
+done
+WITH_MOCK=${WITH_MOCK-true}
+
+if [ "${WITH_MOCK}" = "true" ]; then
+    ./run_dynamodb.sh &
+fi
 # wait start
 #sleep 5
 #python -m pytest -s tests/test_api_task.py::test_tasks_put
 python -m pytest -s
 
-ps -ef | grep "serverless\sdynamodb\sstart" | grep -v grep | awk '{ print $2 }' | xargs kill
-ps -ef | grep DynamoDBLocal_lib | grep -v grep | awk '{ print $2 }' | xargs kill
+if [ "${WITH_MOCK}" = "true" ]; then
+    ps -ef | grep "serverless\sdynamodb\sstart" | grep -v grep | awk '{ print $2 }' | xargs kill
+    ps -ef | grep DynamoDBLocal_lib | grep -v grep | awk '{ print $2 }' | xargs kill
+fi
